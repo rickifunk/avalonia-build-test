383e95a59e70a43a20970fd1da9ea916b33d56ce
Ricki <ricki_ricko2@hotmail.com>
2022-08-22 18:15:42 +0200

    .

---
+++ ProjectSolution/Project/ViewModels/ViewModelBase.cs
@@ -0,0 +1,11 @@
+﻿using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
